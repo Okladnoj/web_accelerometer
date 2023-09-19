ab9717cbaf1b716f134f052cc9933c2822fbe8b7
Okladnoj <dmytrijokladnoj@gmail.com>
2023-09-19 04:31:50 +0300

    Added setProperty

--- lib/src/web/accelerometer_web.dart
+++ lib/src/web/accelerometer_web.dart
@@ -2,8 +2,9 @@
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
-import 'dart:html';
+import 'dart:developer';
+import 'dart:html' as html show window, Accelerometer, DeviceMotionEvent;
+import 'dart:js_util';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -22,15 +23,54 @@ class WebAccelerometerWeb extends AccelerometerPlatform {
 
   @override
   void motionEvents(ValueSetter<AccelerometerData> onMotion) {
+    try {
+      _genericSensorAPI(onMotion);
+    } catch (e) {
+      log('Accelerometer not supported: $e');
+      _motionEvents(onMotion);
+    }
+  }
+
+  void _genericSensorAPI(ValueSetter<AccelerometerData> onMotion) {
+    final accelerometer = html.Accelerometer({'frequency': 60});
+
+    accelerometer.addEventListener('reading', (event) {
+      final data = AccelerometerData(
+        x: (accelerometer.x ?? 0).toDouble(),
+        y: (accelerometer.y ?? 0).toDouble(),
+        z: (accelerometer.z ?? 0).toDouble(),
+      );
+      onMotion(data);
+    });
+
+    setProperty(
+      accelerometer,
+      'onreading',
+      allowInterop(
+        (_) {
+          final data = AccelerometerData(
+            x: (accelerometer.x ?? 0).toDouble(),
+            y: (accelerometer.y ?? 0).toDouble(),
+            z: (accelerometer.z ?? 0).toDouble(),
+          );
+          onMotion(data);
+        },
+      ),
+    );
+
+    accelerometer.start();
+  }
+
+  void _motionEvents(ValueSetter<AccelerometerData> onMotion) {
     html.window.addEventListener('devicemotion', (event) {
-      final motionEvent = event;
-      if (motionEvent is! DeviceMotionEvent) return;
+      final motionEvent = event as html.DeviceMotionEvent;
 
       final data = AccelerometerData(
         x: (motionEvent.acceleration?.x ?? 0).toDouble(),
         y: (motionEvent.acceleration?.y ?? 0).toDouble(),
         z: (motionEvent.acceleration?.z ?? 0).toDouble(),
       );
+
       onMotion(data);
     });
   }
